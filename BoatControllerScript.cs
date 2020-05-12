e0a56960c23ad197fde21d994c28a1ea3adc1f36
Yassine Boumaiza <31522077+mediamax07@users.noreply.github.com>
2020-05-12 21:08:11 +0100

    Add files via upload

---
+++ BoatControllerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (FloatObjectScript))]
+public class BoatControllerScript : MonoBehaviour {
+	public Vector3 COM;
+	[Space (15)]
+	public float speed = 1.0f;
+	public float steerSpeed = 1.0f;
+	public float movementThresold = 10.0f;
+
+	Transform m_COM;
+	float verticalInput;
+	float movementFactor;
+	float horizontalInput;
+	float steerFactor;
+	
+	// Update is called once per frame
+	void Update () {
+		Balance ();
+		Movement ();
+		Steer ();
+	}
+
+	void Balance () {
+		if (!m_COM) {
+			m_COM = new GameObject ("COM").transform;
+			m_COM.SetParent (transform);
+		}
+
+		m_COM.position = COM;
+		GetComponent<Rigidbody> ().centerOfMass = m_COM.position;
+	}
+
+	void Movement () {
+		verticalInput = Input.GetAxis ("Vertical");
+		movementFactor = Mathf.Lerp (movementFactor, verticalInput, Time.deltaTime / movementThresold);
+		transform.Translate (0.0f, 0.0f, movementFactor * speed);
+	}
+
+	void Steer () {
+		horizontalInput = Input.GetAxis ("Horizontal");
+		steerFactor = Mathf.Lerp (steerFactor, horizontalInput * verticalInput, Time.deltaTime / movementThresold);
+		transform.Rotate (0.0f, steerFactor * steerSpeed, 0.0f);
+	}
+}
---
+++ FloatObjectScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class ObjectFloatScript : MonoBehaviour {
+	public float waterLevel = 0.0f;
+	public float floatThreshold = 2.0f;
+	public float waterDensity = 0.125f;
+	public float downForce = 4.0f;
+
+	private float forceFactor;
+	private Vector3 floatForce;
+
+	void FixedUpdate () {
+		forceFactor = 1.0f - ((transform.position.y - waterLevel) / floatThreshold);
+
+		if (forceFactor > 0.0f) {
+			floatForce = -Physics.gravity * GetComponent<Rigidbody> ().mass * (forceFactor - GetComponent<Rigidbody> ().velocity.y * waterDensity);
+			floatForce += new Vector3 (0.0f, -downForce * GetComponent<Rigidbody> ().mass, 0.0f);
+			GetComponent<Rigidbody> ().AddForceAtPosition (floatForce, transform.position);
+		}
+	}
+}
